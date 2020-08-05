d28830a46b391a6857f9fc2ffd1240f26b0564a6
LinJY0424 <f0983344166@gmail.com>
2020-08-05 15:44:13 +0800

    use viewDidLoad to type code

--- CAGradientLayerbyJY/ViewController.swift
+++ CAGradientLayerbyJY/ViewController.swift
@@ -12,8 +12,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [CGColor(red: 56/255, green: 253/255, blue: 230/255, alpha: 1),CGColor(red: 218/255, green: 1, blue: 1, alpha: 1),CGColor(red: 218/255, green: 211/55, blue: 1, alpha: 1),CGColor(red: 218/255, green: 121/255, blue: 1, alpha: 1)]
+        view.layer.insertSublayer(gradientLayer, at: 0)
+        
+        let imageView = UIImageView(image: UIImage(named: "dino"))
+        view.addSubview(imageView)
+        let oneDegree = CGFloat.pi / 180
+        
+        imageView.transform = CGAffineTransform.identity.translatedBy(x: 100, y: 300).scaledBy(x: 2, y: 2).rotated(by: oneDegree*30)
+        
+        
     }
 
-
 }
+
+
 
